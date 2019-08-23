3520643d089e79e3ffdc8cfe11abf572ca309d80
Vanro77 <51413940+Vanro77@users.noreply.github.com>
2019-08-23 09:54:57 -0400

    Add files via upload
    Restrictions on pawns

--- Chess_interface.py
+++ Chess_interface.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 
 class Interface:
@@ -32,6 +33,7 @@ class Interface:
         self.frame_black.pack(side=TOP)
         self.table.pack(expand=1)
         self.frame_white.pack(side=BOTTOM)
+        self.pionnoir = PhotoImage(file='Ressources/pionnoir.gif')
         self.founoir = PhotoImage(file='Ressources/founoir.gif')
         self.foublanc = PhotoImage(file='Ressources/foublanc.gif')
         self.cavalierblanc = PhotoImage(file='Ressources/cavalierblanc.gif')
@@ -44,8 +46,8 @@ class Interface:
         self.tourblanc = PhotoImage(file='Ressources/tourblanc.gif')
         self.reineblanc = PhotoImage(file='Ressources/reineblanc.gif')
         self.reinenoir = PhotoImage(file='Ressources/reinenoir.gif')
-        self.listenoir = []
-        self.listeblanc = []
+        self.listenoir = {}
+        self.listeblanc = {}
 
     def construction(self):
         colonne = 0
@@ -77,44 +79,44 @@ class Interface:
         for casei in self.table.grid_slaves():
             if casei['text'] in ['13', '16']:
                 casei['image'] = self.founoir
-                self.listenoir.append(casei['image'])
+                self.listenoir['fou'] = casei['image']
             elif casei['text'] in ['11', '18']:
                 casei['image'] = self.tournoir
-                self.listenoir.append(casei['image'])
+                self.listenoir['tour'] = casei['image']
             elif casei['text'] == '14':
                 casei['image'] = self.reinenoir
-                self.listenoir.append(casei['image'])
+                self.listenoir['reine'] = casei['image']
             elif casei['text'] == '15':
                 casei['image'] = self.roinoir
-                self.listenoir.append(casei['image'])
+                self.listenoir['roi'] = casei['image']
             elif casei['text'][0] == '2':
                 casei['image'] = self.pionnoir
-                self.listenoir.append(casei['image'])
+                self.listenoir['pion'] = casei['image']
             elif casei['text'] in ['12', '17']:
                 casei['image'] = self.cavaliernoir
-                self.listenoir.append(casei['image'])
+                self.listenoir['cavalier'] = casei['image']
             elif casei['text'] in ['83', '86']:
                 casei['image'] = self.foublanc
-                self.listeblanc.append(casei['image'])
+                self.listeblanc['fou'] = casei['image']
             elif casei['text'] in ['81', '88']:
                 casei['image'] = self.tourblanc
-                self.listeblanc.append(casei['image'])
+                self.listeblanc['tour'] = casei['image']
             elif casei['text'] == '84':
                 casei['image'] = self.reineblanc
-                self.listeblanc.append(casei['image'])
+                self.listeblanc['reine'] = casei['image']
             elif casei['text'] == '85':
                 casei['image'] = self.roiblanc
-                self.listeblanc.append(casei['image'])
+                self.listeblanc['roi'] = casei['image']
             elif casei['text'][0] == '7':
                 casei['image'] = self.pionblanc
-                self.listeblanc.append(casei['image'])
+                self.listeblanc['pion'] = casei['image']
             elif casei['text'] in ['82', '87']:
                 casei['image'] = self.cavalierblanc
-                self.listeblanc.append(casei['image'])
+                self.listeblanc['cavalier'] = casei['image']
             else:
                 casei['image'] = ''
-            self.entry_black['state'] = DISABLED
-            self.entry_white['state'] = NORMAL
+        self.entry_black['state'] = DISABLED
+        self.entry_white['state'] = NORMAL
 
     def mouvement_noir(self):
         b1 = self.Move_black1.get()
@@ -122,18 +124,19 @@ class Interface:
         if int(b1) in range(11, 89) and int(b2) in range(11, 89):
             liste_de_mouvement = []
             for casei in self.table.grid_slaves():
-                if casei['image'] in self.listenoir:
+                if casei['image'] in self.listenoir.values():
                     if casei['text'] == b1:
                         liste_de_mouvement.append(casei)
             for casei in self.table.grid_slaves():
-                if liste_de_mouvement != [] and casei['image'] not in self.listenoir:
+                if liste_de_mouvement != [] and casei['image'] not in self.listenoir.values():
                     if casei['text'] == b2:
                         liste_de_mouvement.append(casei)
             if len(liste_de_mouvement) == 2:
-                liste_de_mouvement[1]['image'] = liste_de_mouvement[0]['image']
-                liste_de_mouvement[0]['image'] = ''
-                self.entry_black['state'] = DISABLED
-                self.entry_white['state'] = NORMAL
+                if self.contrainte_de_déplacement(liste_de_mouvement[0], liste_de_mouvement[1]) is True:
+                    liste_de_mouvement[1]['image'] = liste_de_mouvement[0]['image']
+                    liste_de_mouvement[0]['image'] = ''
+                    self.entry_black['state'] = DISABLED
+                    self.entry_white['state'] = NORMAL
 
     def mouvement_blanc(self):
         w1 = self.Move_white1.get()
@@ -141,37 +144,56 @@ class Interface:
         if int(w1) in range(11, 89) and int(w2) in range(11, 89):
             liste_de_mouvement = []
             for casei in self.table.grid_slaves():
-                if casei['image'] in self.listeblanc:
+                if casei['image'] in self.listeblanc.values():
                     if casei['text'] == w1:
                         liste_de_mouvement.append(casei)
             for casei in self.table.grid_slaves():
-                if liste_de_mouvement != [] and casei['image'] not in self.listeblanc:
+                if liste_de_mouvement != [] and casei['image'] not in self.listeblanc.values():
                     if casei['text'] == w2:
                         liste_de_mouvement.append(casei)
             if len(liste_de_mouvement) == 2:
-                if self.contrainte_de_déplacement(liste_de_mouvement[0], liste_de_mouvement[1]) == 'ok':
+                if self.contrainte_de_déplacement(liste_de_mouvement[0], liste_de_mouvement[1]) is True:
                     liste_de_mouvement[1]['image'] = liste_de_mouvement[0]['image']
                     liste_de_mouvement[0]['image'] = ''
                     self.entry_white['state'] = DISABLED
                     self.entry_black['state'] = NORMAL
+                else:
+                    messagebox.showerror('Erreur', 'Mouvement Impossible')
 
     def contrainte_de_déplacement(self, pointa, pointb):
-        if pointa['image'] == self.pionnoir:
+        if pointa['image'] == self.listenoir['pion']:
             if pointb['image'] == '':
+                if int(pointa['text']) in [21, 22, 23, 24, 25, 26, 27, 28]:
+                    if int(pointb['text']) - int(pointa['text']) == 20:
+                        return True
                 if int(pointb['text']) - int(pointa['text']) == 10:
-                    return 'ok'
-            else:
-                if int(pointb['text']) - int(pointb['text']) == 11:
-                    return 'ok'
-        if pointa['image'] == self.pionblanc:
-            print('ok1')
+                    return True
+                return False
+            if pointb['image'] in self.listeblanc.values():
+                if int(pointb['text'][0]) - int(pointa['text'][0]) == 1:
+                    if abs(int(pointb['text'][1]) - int(pointa['text'][1])) == 1:
+                        return True
+                return False
+        if pointa['image'] == self.listeblanc['pion']:
             if pointb['image'] == '':
-                print('ok2')
+                if int(pointa['text']) in [71, 72, 73, 74, 75, 76, 77, 78]:
+                    if int(pointb['text']) - int(pointa['text']) == -20:
+                        return True
                 if int(pointb['text']) - int(pointa['text']) == -10:
-                    return 'ok'
-            else:
-                if int(pointb['text']) - int(pointb['text']) == -11:
-                    return 'ok'
+                    return True
+                return False
+            if pointb['image'] in self.listenoir.values():
+                if int(pointb['text'][0]) - int(pointa['text'][0]) == -1:
+                    if abs(int(pointb['text'][1]) - int(pointa['text'][1])) == 1:
+                        return True
+                return False
+        if pointa['image'] == self.listeblanc['fou']:
+            if pointb['image'] == '':
+                if abs(int(pointb['text'][0]) - int(pointa['text'][0])) == abs(int(pointb['text'][1]) - int(pointa['text'][1])):
+                    return True
+                return False
+
+
 
 
 
